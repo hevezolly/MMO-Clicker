818a6e81db5a7a3b77d44eb8987300c584cfe264
Titankrot <50486923+Titankrot@users.noreply.github.com>
2021-10-07 15:30:36 +0500

    Spring init

---
+++ src/test/java/ru/matmex/project/MmoClickerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.matmex.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmoClickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
