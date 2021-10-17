94f28805ee96477ca3081b56a1341ef8a79715fe
hevezolly <sevafinogenov@gmail.com>
2021-10-17 19:52:52 +0500

    team and user

---
+++ src/main/java/com/clicker/Clicker/db/Team.java
@@ -0,0 +1,22 @@
+package com.clicker.Clicker.db;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="Teams")
+public class Team {
+
+    @Id
+    private int id;
+
+    @Column(name="click_count")
+    private int click_count;
+
+    @OneToOne
+    @JoinColumn(name="admin")
+    private User admin;
+
+    @OneToMany(mappedBy="current_team")
+    private List<User> users;
+}
---
+++ src/main/java/com/clicker/Clicker/db/User.java
@@ -0,0 +1,24 @@
+package com.clicker.Clicker.db;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Users")
+public class User {
+
+    @Id
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="team_id")
+    private Team current_team;
+
+    @Column(name="click_count")
+    private int clickCount;
+
+    @Column(name="password")
+    private String password;
+}
