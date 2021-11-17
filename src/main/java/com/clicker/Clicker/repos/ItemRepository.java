6ebe50407a99a7ffdee81fe1a3ae50c75e9c74b6
hevezolly <sevafinogenov@gmail.com>
2021-11-17 15:46:24 +0500

    Update ItemRepository.java

--- src/main/java/com/clicker/Clicker/repos/ItemRepository.java
+++ src/main/java/com/clicker/Clicker/repos/ItemRepository.java
@@ -4,6 +4,10 @@ import com.clicker.Clicker.entities.Team;
 import com.clicker.Clicker.entities.items.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemRepository extends JpaRepository<Item, Integer> {
+import java.util.List;
 
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+    List<Item> findByForTeams(boolean forTeams);
 }
+
+
